b2e17ab7b71e5020fdfd7d04c9ca857420c1fc2d
Antoxa152 <benderr@bk.ru>
2026-09-22 17:06:30 +0300

    Remove test, locators, page, conftest.py from  maim branch

--- locators/general_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class GeneralLocators:
-    COOKIES_ACCEPT_BUTTON = [By.XPATH, ".//button[text()='да все привыкли']"]
-    BODY = [By.TAG_NAME, "body"]
-
